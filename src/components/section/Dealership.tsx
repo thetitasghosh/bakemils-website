308ba317416751d701dc68e40ce2a93e157adb02
thetitasghosh <rg814602@gmail.com>
2026-05-06 09:55:59 +0530

    rm red

--- src/components/section/Dealership.tsx
+++ src/components/section/Dealership.tsx
@@ -64,7 +64,7 @@ export default function Dealership() {
           </a>
         </motion.div>
 
-        <div className="flex flex-col md:flex-row items-start justify-center gap-4 red">
+        <div className="flex flex-col md:flex-row items-start justify-center gap-4 redd">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
